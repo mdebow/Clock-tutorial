02fd2e86d1deb21263686d6f0317c1e578ec3378
Unknown <matt.debow@gmail.com>
2017-02-02 13:22:13 -0500

    added code to spin clock

---
+++ Assets/Clock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+
+	Quaternion euler = Quaternion.Euler(
+		new Vector3(0f, 0f, Time.time * 360f ) );
+
+	transform.localRotation = euler;
+
+	}
+}
