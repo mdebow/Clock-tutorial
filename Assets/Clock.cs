698480f9189b10a8a3df78147dd5a239a08a1e59
Unknown <matt.debow@gmail.com>
2017-02-09 12:18:49 -0500

    added code for minutes and hours

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -24,7 +24,13 @@ public class Clock : MonoBehaviour
 		Quaternion secondsRotationQuat = Quaternion.Euler(secondsRotation);
 		secondHand.transform.rotation = secondsRotationQuat;
 	
+		int minutes = time.Minute;
+		Vector3 minutesEuler = new Vector3 (0f, 0f, (1f - (minutes / 60f)) * 360f);
+		MinuteHand.transform.rotation = Quaternion.Euler (minutesEuler);
 
+
+		hourHand.transform.rotation = Quaternion.Euler(
+			Vector3.forward * (1f - (time.Hour / 12f) * 360f)); 
 
         
 		
